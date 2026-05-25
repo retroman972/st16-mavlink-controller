4ab9c8e2ad31507cf182f4c4cd9c9a99fe3dba78
retroman972 <35507602+retroman972@users.noreply.github.com>
2026-05-25 12:19:24 -0400

    Add settings.gradle.kts

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "ST16 MAVLink Controller"
+include(":app")
